1dfb0b04b8127ddb28f082e5280c6439c590a56b
marlon.funk <marlon.funk@web.de>
2023-01-11 21:00:53 +0100

    changed values, added comments

--- analysis.jl
+++ analysis.jl
@@ -1,7 +1,11 @@
 graphs = ["dolphins.net", "20x2+5x2.net", "256_4_4_2_15_18_p.net", "256_4_4_4_13_18_p.net", "adjnoun.net", "cat_cortex_sim.net", "circle9.net", "clique_stars.net", "cliques_line.net", "graph3+1+3.net", "graph3+2+3.net", "grid-6x6.net", "grid-p-6x6.net", "qns04_d.net", "rb25.net", "rb125.net", "rhesus_simetrica.net", "star.net", "wheel.net", "zachary_unwh.net"]
-population_sizes = [10, 100, 1000]
-number_of_generations = [10, 100, 1000]
-amount_of_mutations = [1, 2, 3, 10]
+population_sizes = [25, 50, 100]
+# In the beginning everything is total random, at a random point the fitness increases fast until it reaches the maximum.
+# The beginning can be < 50 or > 500. So a high fitness can appear befor 50 generations or after 500.
+number_of_generations = [100, 500, 1000]
+# Playing around with mutations resulted in the knowledge, that a number > 3 has a bad impact on the result
+#   which makes sense, because the amount of randomness is increased and the impact of the fittest individuals is decreased
+amount_of_mutations = [1, 2, 3] 
 selection_function = ["tournament", "roulette"]
 crossover_function = ["one_point", "uniform"]
 
